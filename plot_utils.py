9d4cd586124a1318e1f8ecfdcb358915c645c67f
lucas.a.alcantara <lucas.a.alcantara@accenture.com>
2022-06-29 20:40:18 -0300

    fix broken dependencies, fix image plot, tsne plot

--- plot_utils.py
+++ plot_utils.py
@@ -1,6 +1,8 @@
 import numpy as np
-from scipy.misc import imsave
-from scipy.misc import imresize
+from imageio import imwrite
+from skimage.transform import resize
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # this function is borrowed from https://github.com/hwalsuklee/tensorflow-mnist-AAE/blob/master/plot_utils.py
 class plot_samples():
@@ -16,7 +18,7 @@ class plot_samples():
 
     def save_images(self, images, name='result.jpg'):
         images = images.reshape(self.n_img_x*self.n_img_y, self.img_h, self.img_w)
-        imsave(self.DIR + "/"+name, self._merge(images, [self.n_img_y, self.n_img_x]))
+        imwrite(self.DIR + "/"+name, self._merge(images, [self.n_img_y, self.n_img_x]))
 
     def _merge(self, images, size):
         h, w = images.shape[1], images.shape[2]
@@ -26,6 +28,21 @@ class plot_samples():
         for idx, image in enumerate(images):
             i = int(idx % size[1])
             j = int(idx / size[1])
-            image_ = imresize(image, size=(w,h), interp='bicubic')
+            image_ = resize(image, output_shape=(w,h))
             img[j*h:j*h+h, i*w:i*w+w] = image_
+        img = img * 255
+        img = img.astype(np.uint8)
         return img
+
+def tsne_plot(tsne_results, labels, path, title=''):
+    plt.figure(figsize=(16, 10))
+    sns.scatterplot(x=tsne_results[:, 0],
+                    y=tsne_results[:, 1],
+                    hue=labels,
+                    legend='full',
+                    palette=sns.color_palette('hls', len(set(labels))))
+    plt.title(title)
+    plt.savefig(dpi=150,
+                fname=path,
+                bbox_inches='tight')
+    plt.close()
