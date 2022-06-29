f3b116398b861b28cb383e663c436ab557ee2051
lucas.a.alcantara <lucas.a.alcantara@accenture.com>
2022-06-29 14:00:34 -0300

    dissimilarity matrix with mse

--- losses.py
+++ losses.py
@@ -17,6 +17,19 @@ def get_dissimilarity_matrix(input, sigma=2, beta=0):
     return input - beta
 
 
+def get_mse_dissimilarity_matrix(input, beta=0):
+    """
+    Using MSE to calculate the distance between the feature vectors
+      k(x, y) = ||x-y||_2^2;
+      x -> (k(c_1, x), ..., k(c_m, x)), c_i \in centers, \forall i.
+    """
+    input = input.sub(input.unsqueeze(1)).pow(2).view(input.size(0),
+                                                      input.size(0), -1)
+    input = input.mean(dim=-1)
+
+    return input - beta
+
+
 def get_pairwise_labels_matrix(targets, n_classes):
     onehot_targets = torch.nn.functional.one_hot(targets, n_classes)
     onehot_targets = onehot_targets.float()
@@ -29,7 +42,7 @@ def get_pairwise_labels_matrix(targets, n_classes):
 def nmse(input, targets, n_classes, neo=False):
     loss_fn = torch.nn.MSELoss(reduction='mean')
 
-    x = get_dissimilarity_matrix(input, beta=-1)
+    x = get_mse_dissimilarity_matrix(input, beta=0)
     x = x.view([-1] + list(x.size())[2:])
 
     y = get_pairwise_labels_matrix(targets, n_classes=n_classes)
@@ -50,7 +63,7 @@ def contrastive(input, targets, n_classes, neo=False):
     x = torch.where(
         torch.eye(len(input)).to(input.device) == 1,
         torch.tensor(-float('inf')).to(input.device),
-        get_dissimilarity_matrix(input))  # removes the main diagonal
+        get_mse_dissimilarity_matrix(input))  # removes the main diagonal
     x = x.view([-1] + list(x.size())[2:])
 
     y = get_pairwise_labels_matrix(targets, n_classes=n_classes)
